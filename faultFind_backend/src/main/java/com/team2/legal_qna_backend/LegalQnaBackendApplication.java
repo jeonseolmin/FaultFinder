9c2886c1094969b39f1993d9fd3620b281bde292
zmxn9941@naver.com <zmxn9941@naver.com>
2026-06-08 12:23:44 +0900

    frist 기본 틀

---
+++ faultFind_backend/src/main/java/com/team2/legal_qna_backend/LegalQnaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team2.legal_qna_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalQnaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalQnaBackendApplication.class, args);
+	}
+
+}
---
+++ faultFind_backend/src/test/java/com/team2/legal_qna_backend/LegalQnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.legal_qna_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalQnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
