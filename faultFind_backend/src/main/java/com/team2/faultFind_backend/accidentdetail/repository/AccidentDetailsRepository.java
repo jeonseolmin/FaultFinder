91b6ac0c305f79b838a3579684666f4806756fc0
zmxn9941@naver.com <zmxn9941@naver.com>
2026-06-10 15:47:48 +0900

    fix : details 수정

--- faultFind_backend/src/main/java/com/team2/faultFind_backend/accidentdetail/repository/AccidentDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team2.faultFind_backend.accidentdetail.repository;
-import com.team2.faultFind_backend.accidentdetail.entity.AccidentDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccidentDetailsRepository extends JpaRepository<AccidentDetails, Long> {
-
-}
-
