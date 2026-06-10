f04fae790015b5de6b8cce78595a1782c5c8c4ef
Navii112 <hyun010902@gmail.com>
2026-06-10 15:51:28 +0900

    fix : service 수정

--- faultFind_backend/src/main/java/com/team2/faultFind_backend/accidentdetail/repository/AccidentDetailsRepository.java
+++ faultFind_backend/src/main/java/com/team2/faultFind_backend/accidentdetail/repository/AccidentDetailsRepository.java
@@ -1,7 +1,6 @@
 package com.team2.faultFind_backend.accidentdetail.repository;
 import com.team2.faultFind_backend.accidentdetail.entity.AccidentDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,10 +8,6 @@ import java.util.Optional;
 @Repository
 public interface AccidentDetailsRepository extends JpaRepository<AccidentDetails, Long> {
     Optional<AccidentDetails> findByCaseCode(String caseCode);
-=======
 
-public interface AccidentDetailsRepository extends JpaRepository<AccidentDetails, Long> {
-
->>>>>>> 53a12256c8996c5ba4aa0b2404a7f1b1063776d8
 }
 
