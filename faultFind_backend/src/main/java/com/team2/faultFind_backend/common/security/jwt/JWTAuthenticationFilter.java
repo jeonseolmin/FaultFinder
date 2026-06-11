ef8b7b0ff3aceb1e028a6b29bb06dfeb6b50ae0b
zmxn9941@naver.com <zmxn9941@naver.com>
2026-06-11 16:08:27 +0900

    feat (전설민) : 작업 중 modifier 테이블의 chage_value 값 자료형 Integer로 변경 JWT 기본 구조와 경로 인가 작업 완료 후 POST MAN으로 TEST 작업 시작

--- faultFind_backend/src/main/java/com/team2/faultFind_backend/common/security/jwt/JWTAuthenticationFilter.java
+++ faultFind_backend/src/main/java/com/team2/faultFind_backend/common/security/jwt/JWTAuthenticationFilter.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
