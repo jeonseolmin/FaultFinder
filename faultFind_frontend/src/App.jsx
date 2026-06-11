b34a05ace784f90cb39dc2a75652b52729a26146
DESKTOP-85ISA1V\human-11 <kimheajeong0510@gmail.com>
2026-06-11 12:05:47 +0900

    feat: Heder 완성

--- faultFind_frontend/src/App.jsx
+++ faultFind_frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
+import Heder from './components/Heder/Heder';
+
 // 메인 App 조합
 export default function App() {
   return (
    <>
     <Navbar/>
+    <Heder />
+    
    </>
   );
 }
