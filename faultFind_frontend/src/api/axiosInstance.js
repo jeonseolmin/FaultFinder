dffea7eb30d1649fd78e7753f02f7a721a9cc743
DESKTOP-1MQFEV7\junse <junseolmin91@naver.com>
2026-06-27 19:04:15 +0900

    도커 설정 중

--- faultFind_frontend/src/api/axiosInstance.js
+++ faultFind_frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://3.27.17.82:8080",
 });
 
 axiosInstance.interceptors.request.use((config) => {
